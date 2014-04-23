c60fbdc790f8cb3172ee2dab64f0343917caa084
Jakub Jirutka <jakub@jirutka.cz>
2014-04-23 14:04:37 +0200

    Autoconvert Markdown to RST in setup

--- setup.py
+++ setup.py
@@ -1,17 +1,36 @@
-from distutils.core import setup
+#!/usr/bin/env python3
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
 
 setup(
     name='haste-client',
     version='1.0.1',
-    scripts=['haste'],
     url='https://github.com/jirutka/haste-client',
-    license='MIT',
+    description='CLI client for Haste server (hastebin.com) written in Python.',
+    long_description=read_md('README.md'),
     author='Jakub Jirutka',
     author_email='jakub@jirutka.cz',
-    description='CLI client for Haste server (hastebin.com) written in Python.',
-    long_description=open('README.md').read(),
-    requires=[
+    license='MIT',
+    scripts=['haste'],
+    install_requires=[
         'requests',
         'docopt'
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
     ]
 )
